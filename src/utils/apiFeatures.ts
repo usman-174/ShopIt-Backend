14b1bc522a452348aefde4da0dc234e44ce4943d
Muhmmad Usman <hellmughal123@gmail.com>
2021-09-21 17:47:37 +0500

    tea2

--- src/utils/apiFeatures.ts
+++ src/utils/apiFeatures.ts
@@ -13,30 +13,33 @@ export class ApiFeatures {
             }
         } : {}
 
-        this.query = this.query.find({ ...keyword }) 
-        return this
+        this.query = this.query.find({ ...keyword });
+        return this;
     }
 
     filter() {
 
-        let queryCopy = { ...this.queryStr }
-        const removeField = ['keyword', 'limit', 'page']
-        removeField.forEach(field => delete queryCopy[field])
-        
-        queryCopy = JSON.stringify(queryCopy)
-        queryCopy = queryCopy.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+        const queryCopy = { ...this.queryStr };
 
-        queryCopy = this.query.find(JSON.parse(queryCopy))
-        return this
+        // Removing fields from the query
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el]);
+
+        // Advance filter for price, ratings etc
+        let queryStr = JSON.stringify(queryCopy)
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
 
 
+        this.query = this.query.find(JSON.parse(queryStr));
+        return this;
     }
-    pagination(resultPerPage:number) {
-        const currentPage=Number(this.queryStr.page) || 1
-        const toSkip = resultPerPage * (currentPage-1) 
 
-        this.query = this.query.limit(resultPerPage).skip(toSkip)
-        return this
+    pagination(resPerPage) {
+        const currentPage = Number(this.queryStr.page) || 1;
+        const skip = resPerPage * (currentPage - 1);
+
+        this.query = this.query.limit(resPerPage).skip(skip);
+        return this;
     }
 }
 
