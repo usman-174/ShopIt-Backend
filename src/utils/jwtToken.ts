11d73b4b9fab1b76985530657793813b95d6f3c8
usman-174 <hellmughal123@gmail.com>
2021-10-07 00:50:18 +0500

    Set Cookie Site to None2

--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -4,12 +4,12 @@ import { IUser } from "../interfaces/userI";
 export const sendToken = (user:IUser,statusCode:number,res:Response)=>{
     const token = user.getJwtToken()
     user.password = undefined
+    user.resetPasswordExpire = undefined
     const options :CookieOptions= {
         expires : new Date(Date.now() + parseInt(String(process.env.COOKIE_EXPIRES_TIME)) * 24*60*60 *1000),
         httpOnly:true, 
         sameSite: "none" 
         
     }
-    delete user["resetPasswordExpire"]
     return res.status(statusCode).cookie('token',token,options).json({success:true,user})
 }
