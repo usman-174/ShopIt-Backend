35487d4cb88c34e1bb0ef209f387d2c7adc623c5
usman-174 <hellmughal123@gmail.com>
2021-10-07 01:21:00 +0500

    fixed

--- build/utils/jwtToken.js
+++ build/utils/jwtToken.js
@@ -2,11 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
-    console.log("Helo sending cookie :) ");
     return res.status(statusCode).cookie('token', token, {
         expires: new Date(Date.now() + parseInt(String(process.env.COOKIE_EXPIRES_TIME)) * 24 * 60 * 60 * 1000),
         httpOnly: true,
         sameSite: "none",
+        secure: process.env.NODE_ENV === "production" ? true : false,
         path: "/api",
     }).json({ success: true, user });
 };
--- src/utils/jwtToken.ts
+++ src/utils/jwtToken.ts
@@ -3,12 +3,12 @@ import { IUser } from "../interfaces/userI";
 
 export const sendToken = (user:IUser,statusCode:number,res:Response)=>{
     const token = user.getJwtToken()
-    console.log("Helo sending cookie :) ");
     
     return res.status(statusCode).cookie('token',token,{
         expires : new Date(Date.now() + parseInt(String(process.env.COOKIE_EXPIRES_TIME)) * 24*60*60 *1000),
         httpOnly:true, 
         sameSite: "none",
+        secure : process.env.NODE_ENV === "production"?true:false,
         path:"/api",
          
     }).json({success:true,user})
